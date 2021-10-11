a22a0089c78b94a9f66ceea02643f11b9d5275b5
TiroZit <nikus.master@gmail.com>
2021-10-11 21:26:53 +0500

    products

---
+++ src/components/page-blocks/js/_swiper.js
@@ -0,0 +1,9 @@
+new Swiper('.slider-text', {
+   spaceBetween: 40,
+   simulateTouch: false,
+   touchRatio: 1,
+   touchAngle: 45,
+   grabCursor: true,
+   autoHeight: false,
+   slidesPerView: 1,
+});
